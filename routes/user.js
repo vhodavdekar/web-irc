e82285614509a1a0ea51c231497587a2fb94eabe
Venkatesh Hodavdekar <vhodavdekar@gmail.com>
2013-02-03 10:29:18 -0500

    test server side implementation of event source

--- routes/user.js
+++ routes/user.js
@@ -4,5 +4,22 @@
  */
 
 exports.list = function(req, res){
-  res.send("respond with a resource");
+    res.writeHead(200, {
+	'Content-Type'  : 'text/event-stream',
+	'Cache-Control' : 'no-cache',
+	'Connection'    : 'keep-alive',
+    });
+
+    console.log('Client connect');
+ 
+    var t = setInterval(function () {
+        console.log('Send data');
+        res.write('data: DATA\n\n');
+    }, 1000);
+ 
+    res.socket.on('close', function () {
+        console.log('Client leave');
+        clearInterval(t);
+    });
+
 };
